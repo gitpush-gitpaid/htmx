bb88dcf7c9005c42cb045583f261f2965f433fbd
gitpush-gitpaid <bklynforge@gmail.com>
2023-11-20 19:52:36 -0500

    sam cook ceo

--- script.js
+++ script.js
@@ -60,6 +60,11 @@ document.addEventListener('DOMContentLoaded', () => {
       alt: 'co-ceo of HTMX',
       url: 'https://twitter.com/_abrinsky',
     },
+    {
+      src: 'samcook_ceo.jpg',
+      alt: 'co-ceo of HTMX',
+      url: 'https://twitter.com/samcook_',
+    },
   ];
 
   const shillImages = [
