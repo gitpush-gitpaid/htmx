bf16b09a52493f65007333dc715c184c47e53b08
gitpush-gitpaid <bklynforge@gmail.com>
2023-11-20 19:58:34 -0500

    stauros ceo

--- script.js
+++ script.js
@@ -65,6 +65,11 @@ document.addEventListener('DOMContentLoaded', () => {
       alt: 'co-ceo of HTMX',
       url: 'https://twitter.com/samcook_',
     },
+    {
+      src: 'stauros_ceo.jpg',
+      alt: 'co-ceo of HTMX',
+      url: 'https://twitter.com/olavostauros',
+    },
   ];
 
   const shillImages = [
