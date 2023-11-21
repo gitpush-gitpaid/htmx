7614a88f33eefe179117c71ed9d7adf3992982ee
gitpush-gitpaid <bklynforge@gmail.com>
2023-11-20 19:51:18 -0500

    abrinsky ceo

--- script.js
+++ script.js
@@ -55,6 +55,11 @@ document.addEventListener('DOMContentLoaded', () => {
       alt: 'co-ceo of HTMX',
       url: 'https://twitter.com/trshpuppy',
     },
+    {
+      src: 'abrinsky_ceo.jpg',
+      alt: 'co-ceo of HTMX',
+      url: 'https://twitter.com/_abrinsky',
+    },
   ];
 
   const shillImages = [
