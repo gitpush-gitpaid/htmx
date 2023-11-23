33299fea9b5f08441b244007cfca05d97efb6e19
gitpush-gitpaid <bklynforge@gmail.com>
2023-11-22 23:33:39 -0500

    josh ceo

--- script.js
+++ script.js
@@ -115,6 +115,11 @@ document.addEventListener('DOMContentLoaded', () => {
       alt: 'co-ceo of HTMX',
       url: 'https://twitter.com/magnum_d1ngus',
     },
+    {
+      src: 'joshmanders_ceo.jpg',
+      alt: 'co-ceo of HTMX',
+      url: 'https://twitter.com/joshmanders',
+    },
   ];
 
   const shillImages = [
