0f8741b8d63ed4ec3fa77f04b9013208c29cedfe
gitpushgitpaid <bklynforge@gmail.com>
2023-12-05 09:26:00 -0500

    tokiomute ceo

--- script.js
+++ script.js
@@ -175,6 +175,11 @@ document.addEventListener('DOMContentLoaded', () => {
       alt: 'co-ceo of HTMX',
       url: 'https://twitter.com/atalocke',
     },
+    {
+      src: 'tokiomute_ceo.jpg',
+      alt: 'co-ceo of HTMX',
+      url: 'https://twitter.com/tokiomutex',
+    },
   ];
 
   const shillImages = [
