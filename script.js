0de2bbf36c8cf4da20508ea5b9921c154f4a7ac6
gitpush-gitpaid <bklynforge@gmail.com>
2024-01-15 22:19:11 -0500

    tim_bytes ceo

--- script.js
+++ script.js
@@ -205,7 +205,8 @@ document.addEventListener('DOMContentLoaded', () => {
     'uwunetes',
     '_studable_',
     'phrackery',
-    'BifocalCanvas'
+    'BifocalCanvas',
+    'tim_bytes'
   ];
 
   shuffleArray(ceoImages);
